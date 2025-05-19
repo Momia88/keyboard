17ee472a11427791811212cb6c136f07662731cb
momia88 <tw.simi@gmail.com>
2025-05-19 14:34:06 +0800

    Add files via upload
    Modify code

--- keyboard.go
+++ keyboard.go
@@ -1,5 +1,16 @@
+// Package keyboard provides a function to convert an integer to a float64.
 package keyboard
 
-func Getdouble(num float32) float32 {
-	return num * 2
+import (
+	"strconv"
+)
+
+// Getdouble converts an integer to a float64
+// and returns it. It takes an integer as input.
+func GetFloat(input string) (float64, error) {
+	num, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return num, nil
 }
